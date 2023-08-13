97d9b7f5a4accc65e9ddcd5107ed9d9fb1fcf998
SlimAhmad <slimahmad6@outlook.com>
2023-08-13 16:54:24 +0100

    INFRA: Initialize FlutterWave.Core.Infrastructure.Build

---
+++ FlutterWave.Core.Infrastructure.Build/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+namespace FlutterWave.Core.Infrastructure.Build
+{
+
+
+
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var adoNetClient = new ADotNetClient();
+
+            var githubPipeline = new GithubPipeline
+            {
+                Name = "FlutterWave.Core Build",
+
+
+                OnEvents = new Events
+                {
+                    Push = new PushEvent
+                    {
+                        Branches = new string[] { "main" }
+                    },
+
+                    PullRequest = new PullRequestEvent
+                    {
+                        Branches = new string[] { "main" }
+                    }
+                },
+
+                Jobs = new Jobs
+                {
+                    Build = new BuildJob
+                    {
+                        EnvironmentVariables = new Dictionary<string, string>
+                        {
+                            { "ApiKey", "${{ secrets.APIKEY }}" },
+                            { "EncryptionKey", "${{ secrets.ENCRYPTIONKEY }}" }
+                        },
+
+                        RunsOn = BuildMachines.WindowsLatest,
+
+                        Steps = new List<GithubTask>
+                        {
+                            new CheckoutTaskV2
+                            {
+                                Name = "Pulling Code"
+                            },
+
+                            new SetupDotNetTaskV1
+                            {
+                                Name = "Installing .NET",
+
+                                TargetDotNetVersion = new TargetDotNetVersion
+                                {
+                                    DotNetVersion = "7.0.201"
+                                }
+                            },
+
+                            new RestoreTask
+                            {
+                                Name = "Restoring Packages"
+                            },
+
+                            new DotNetBuildTask
+                            {
+                                Name = "Building Solution"
+                            },
+
+                            new TestTask
+                            {
+                                Name = "Running Tests"
+                            }
+                        }
+                    }
+                }
+            };
+
+            adoNetClient.SerializeAndWriteToFile(
+                adoPipeline: githubPipeline,
+                path: "../../../../.github/workflows/dotnet.yml");
+
+        }
+    }
+}
